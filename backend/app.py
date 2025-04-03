ea39405ab933d8fb9f3a0b9bfbe7d8e0ad98643c
Brayan F Abila Ferrera <67338469+ferchi2001@users.noreply.github.com>
2025-04-02 21:28:42 -0400

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def index():
+    return render_template('chat.html')
+
+@socketio.on('message')
+def handle_message(data):
+    emit('message', data, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=5001)
